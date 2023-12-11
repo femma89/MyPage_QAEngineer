37ba7a16038981c06a0eb85bf1250380f4f7f8e3
Emmanuel Flores <rcoc.ext9@nec.com.ar>
2023-12-11 17:13:47 -0300

    Estructura de Forjando el camino. Select Combo. Redefinicion de tooltip

--- scripts.js
+++ scripts.js
@@ -42,6 +42,18 @@ function showProjectDetails(projectId) {
     proyectoToShow.style.display = 'block';
     proyectoToShow.classList.add('animated-section');
     proyectoToShow.style.animation = 'fade 1.5s ease-in-out'; // Aplica la animación al mostrar la sección
+    
+    // Muestra el combo box solo si se hace clic en "Forjando el camino"
+    if (projectId === 'proyecto1-info') {
+        const selectTimeline = document.getElementById('select-way');
+        selectTimeline.style.display = 'block'; // Muestra el combo box
+        selectTimeline.classList.add('show'); // Asegura que esté visible
+    } else {
+        const selectTimeline = document.getElementById('select-way');
+        selectTimeline.style.display = 'none'; // Oculta el combo box para otras secciones
+        selectTimeline.classList.remove('show');
+    }
+
     hidePresentation();
 }
 
@@ -77,6 +89,57 @@ function showPresentation() {
     }, 100); // Modificar esta cantidad para ajustar la demora, si es necesario
 }
 
+function updateBannerTitle(sectionTitle) {
+    const dynamicTitle = document.getElementById('dynamicTitle');
+    dynamicTitle.textContent = sectionTitle;
+}
+
+document.getElementById('proyectos').addEventListener('click', function(event) {
+    const clickedElement = event.target.textContent;
+    if (clickedElement === 'Forjando el camino' || clickedElement === 'Proyectos') {
+        updateBannerTitle(clickedElement);
+        showBannerTitle();
+    }
+});
+
+document.getElementById('habilidades').addEventListener('click', function(event) {
+    const clickedElement = event.target.textContent;
+    if (clickedElement === 'Trayectoria' || clickedElement === 'Habilidades') {
+        updateBannerTitle(clickedElement);
+        showBannerTitle();
+    }
+});
+
+document.getElementById('personales').addEventListener('click', function(event) {
+    const clickedElement = event.target.textContent;
+    if (clickedElement === 'Perfil' || clickedElement === 'Referencias') {
+        updateBannerTitle(clickedElement);
+        showBannerTitle();
+    }
+});
+
+document.getElementById('contactos').addEventListener('click', function(event) {
+    const clickedElement = event.target.textContent;
+    if (clickedElement === 'Enviar mensaje') {
+        updateBannerTitle(clickedElement);
+        showBannerTitle();
+    }
+});
+
+function showBannerTitle() {
+    const bannerTitle = document.getElementById('dynamicTitle');
+    bannerTitle.style.display = 'block';
+}
+
+// Ocultar el título general al cargar la página
+window.addEventListener('load', function() {
+    const bannerTitle = document.getElementById('dynamicTitle');
+    bannerTitle.style.display = 'none';
+});
+
+
+// Puedes seguir añadiendo más listeners para las otras secciones si las tienes
+
 window.onload = function() {
     var presentacion = document.querySelector('.presentacion');
     presentacion.style.display = 'block';
@@ -86,5 +149,82 @@ window.onload = function() {
         presentacion.classList.add('animated-presentation');
     }, 100); // Modificar esta cantidad para ajustar la demora, si es necesario
 };
+
+document.addEventListener('DOMContentLoaded', function () {
+    const celdas = document.querySelectorAll('.celda');
+    let timeout;
+
+    celdas.forEach(celda => {
+        const tooltip = document.createElement('div');
+        tooltip.classList.add('tooltip');
+        tooltip.textContent = celda.getAttribute('data-tooltip');
+        celda.appendChild(tooltip); // Adjuntamos el tooltip a la celda
+
+
+        celda.addEventListener('mouseenter', function (event) {
+            const rect = this.getBoundingClientRect();
+            const isNearBottom = window.innerHeight - rect.bottom < 100;
+            const isNearTop = rect.top < window.innerHeight / 2; // Verifica si está en la mitad superior
+
+            if (isNearBottom) {
+                tooltip.style.top = 'auto';
+                tooltip.style.bottom = '100%';
+                tooltip.style.transform = 'translate(50%, 20%)';
+            } else if (isNearTop) {
+                tooltip.style.top = '100%';
+                tooltip.style.bottom = 'auto';
+                tooltip.style.transform = 'translate(50%, -20%)'; // Modificación aquí
+            } else {
+                tooltip.style.top = 'auto';
+                tooltip.style.bottom = 'auto';
+                tooltip.style.transform = 'translate(-50%, 0)';
+            }
+
+            timeout = setTimeout(() => {
+                tooltip.style.opacity = 1;
+            }, 0);
+        });
+
+        celda.addEventListener('mouseleave', function (event) {
+            clearTimeout(timeout);
+            tooltip.style.opacity = 0;
+        });
+
+        tooltip.addEventListener('mouseenter', function (event) {
+            clearTimeout(timeout);
+        });
+
+        tooltip.addEventListener('mouseleave', function (event) {
+            tooltip.style.opacity = 0;
+        });
+    });
+});
+
+window.addEventListener('load', function() {
+    // Oculta el combo box al cargar la página
+    const selectTimeline = document.getElementById('select-way');
+    selectTimeline.style.display = 'none';
+
+    // Función para desplazar a la sección seleccionada en el timeline
+    function scrollToElement(elementId) {
+        const element = document.getElementById(elementId);
+        if (element) {
+            element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+    }
+
+    // Manejador de eventos para el cambio en el combo box
+    selectTimeline.addEventListener('change', function() {
+        const selectedValue = this.value;
+        scrollToElement(selectedValue);
+    });
+});
+
+
+
+
+
+
+
 
 
